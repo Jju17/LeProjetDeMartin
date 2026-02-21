7c438bf28adba8cc0b44e61ce9aab544a0dd6ff0
Julien R <julien@rahier.dev>
2026-02-21 12:29:36 +0100

    Initial Commit

---
+++ LeProjetDeMartin/LeProjetDeMartinApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeProjetDeMartinApp.swift
+//  LeProjetDeMartin
+//
+//  Created by Julien on 21/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct LeProjetDeMartinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
